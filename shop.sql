b8279101f323199f01ac7abe29d67d5a76088501
caishaodong <1146631758@qq.com>
2020-11-11 22:46:31 +0800

    update sql file

--- shop.sql
+++ shop.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50641
  File Encoding         : 65001
 
- Date: 11/11/2020 21:41:14
+ Date: 11/11/2020 22:45:08
 */
 
 SET NAMES utf8mb4;
@@ -35,10 +35,13 @@ CREATE TABLE `base`  (
 DROP TABLE IF EXISTS `c_sys_menu`;
 CREATE TABLE `c_sys_menu`  (
   `id` bigint(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '菜单名称',
-  `pid` bigint(11) NULL DEFAULT NULL COMMENT '父菜单id',
-  `type` tinyint(1) NULL DEFAULT 1 COMMENT '类型（1：菜单，2：按钮）',
-  `is_enable` tinyint(1) NULL DEFAULT 1 COMMENT '是否启用（0：禁用，1：启用）',
+  `menu_name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '菜单名称',
+  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '地址',
+  `pid` bigint(11) UNSIGNED NULL DEFAULT NULL COMMENT '父菜单id',
+  `menu_type` tinyint(1) UNSIGNED NOT NULL DEFAULT 1 COMMENT '类型（1：菜单，2：按钮）',
+  `level` tinyint(1) UNSIGNED NOT NULL COMMENT '级别（1：一级，2：二级，3：三级）',
+  `sort` int(8) UNSIGNED NOT NULL DEFAULT 999 COMMENT '排序',
+  `is_enable` tinyint(1) NOT NULL DEFAULT 1 COMMENT '是否启用（0：禁用，1：启用）',
   `is_deleted` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否删除（0：未删除，1：删除）',
   `gmt_create` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   `gmt_modified` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '修改时间',
@@ -51,7 +54,8 @@ CREATE TABLE `c_sys_menu`  (
 DROP TABLE IF EXISTS `c_sys_role`;
 CREATE TABLE `c_sys_role`  (
   `id` bigint(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '角色名称',
+  `role_name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '角色名称',
+  `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '角色备注',
   `is_deleted` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否删除（0：未删除，1：删除）',
   `gmt_create` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   `gmt_modified` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '修改时间',
@@ -64,8 +68,8 @@ CREATE TABLE `c_sys_role`  (
 DROP TABLE IF EXISTS `c_sys_role_menu`;
 CREATE TABLE `c_sys_role_menu`  (
   `id` bigint(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `sys_role_id` bigint(11) NULL DEFAULT NULL COMMENT '系统角色id',
-  `sys_menu_id` bigint(11) NULL DEFAULT NULL COMMENT '系统菜单id',
+  `sys_role_id` bigint(11) UNSIGNED NOT NULL COMMENT '系统角色id',
+  `sys_menu_id` bigint(11) UNSIGNED NOT NULL COMMENT '系统菜单id',
   `is_deleted` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否删除（0：未删除，1：删除）',
   `gmt_create` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   `gmt_modified` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '修改时间',
@@ -79,11 +83,11 @@ CREATE TABLE `c_sys_role_menu`  (
 DROP TABLE IF EXISTS `c_sys_user`;
 CREATE TABLE `c_sys_user`  (
   `id` bigint(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '用户名',
-  `password` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '密码',
-  `salt` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '盐值',
-  `mobile` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '手机号',
-  `email` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '邮箱',
+  `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '用户名',
+  `password` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '密码',
+  `salt` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '盐值',
+  `mobile` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '手机号',
+  `email` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '邮箱',
   `is_deleted` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否删除（0：未删除，1：删除）',
   `gmt_create` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   `gmt_modified` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '修改时间',
@@ -99,8 +103,8 @@ CREATE TABLE `c_sys_user`  (
 DROP TABLE IF EXISTS `c_sys_user_role`;
 CREATE TABLE `c_sys_user_role`  (
   `id` bigint(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `sys_user_id` bigint(11) NOT NULL COMMENT '系统用户id',
-  `sys_role_id` bigint(11) NOT NULL COMMENT '系统角色id',
+  `sys_user_id` bigint(11) UNSIGNED NOT NULL COMMENT '系统用户id',
+  `sys_role_id` bigint(11) UNSIGNED NOT NULL COMMENT '系统角色id',
   `is_deleted` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否删除（0：未删除，1：删除）',
   `gmt_create` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   `gmt_modified` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '修改时间',
